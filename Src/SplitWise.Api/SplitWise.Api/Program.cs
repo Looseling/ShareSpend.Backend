9ca17d97b5f5169b37892f5721a843cdc9b01727
Khan <batyrkhan.akzholov@gmail.com>
2024-05-12 19:42:31 +0200

    Add DB Context

--- Src/SplitWise.Api/SplitWise.Api/Program.cs
+++ Src/SplitWise.Api/SplitWise.Api/Program.cs
@@ -1,12 +1,17 @@
 using SplitWise.Application;
 using SplitWise.AddInfrastructure;
+using Microsoft.EntityFrameworkCore;
+using ShareSpend.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 {
-
     builder.Services.AddApplication().
                      AddInfrastructure();
 
+    builder.Services.AddDbContext<AppDbContext>(options =>
+    {
+        options.UseSqlServer(builder.Configuration.GetConnectionString("LocalDevDB"));
+    });
     builder.Services.AddControllers();
 }
 
---
+++ Src/SplitWise.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShareSpend.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShareSpend.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Receipt> Receipts { get; set; }
+        public DbSet<ReceiptItem> ReceiptItems { get; set; }
+        public DbSet<ReceiptContainer> ReceiptContainers { get; set; }
+        public DbSet<UserReceiptContainer> UserReceiptContainers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configure many-to-many relationship
+            modelBuilder.Entity<UserReceiptContainer>()
+                .HasKey(uc => new { uc.UserId, uc.ReceiptContainerId });
+
+            modelBuilder.Entity<UserReceiptContainer>()
+                .HasOne(uc => uc.User)
+                .WithMany(u => u.UserReceiptContainers)
+                .HasForeignKey(uc => uc.UserId);
+
+            modelBuilder.Entity<UserReceiptContainer>()
+                .HasOne(uc => uc.ReceiptContainer)
+                .WithMany(c => c.UserReceiptContainers)
+                .HasForeignKey(uc => uc.ReceiptContainerId);
+        }
+    }
+}
